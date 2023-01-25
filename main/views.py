5b78c8daad3979a8869d286f5ff2ed1372cca82e
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-25 11:57:24 -0500

    sequelite db setup

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDo, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello</h1>")
+def index(response, id):
+    ls = ToDo.objects.get(id=id)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
 
 def v1(response):
     return HttpResponse("<h1>view1</h1>")
