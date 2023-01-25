a5d1367c76e0025cd8a38bd9d7a1588377f0feac
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-25 14:00:26 -0500

    rendering html files

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ToDo, Item
 
 # Register your models here.
+admin.site.register(ToDo)
+admin.site.register(Item)
--- main/views.py
+++ main/views.py
@@ -6,8 +6,10 @@ from .models import ToDo, Item
 
 def index(response, id):
     ls = ToDo.objects.get(id=id)
-    items = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
+    return render(response, "main/list.html", {"ls":ls})
 
-def v1(response):
-    return HttpResponse("<h1>view1</h1>")
+
+
+def home(response):
+    return render(response, "main/home.html", {"name": "test"})
+
